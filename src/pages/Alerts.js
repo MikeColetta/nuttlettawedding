d2f852a2c90da3f9cbfdb4256ca378d887acd70c
Mike Coletta <coletta.mike@gmail.com>
2022-08-05 15:49:17 -0700

    Added cards

--- src/pages/Alerts.js
+++ src/pages/Alerts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 
 
 class Alerts extends React.Component {
@@ -9,10 +9,16 @@ class Alerts extends React.Component {
             <div className="sectionDiv">
                 <Container className="contentContainer">
                     <br></br>
-                    <h2>Covid Information</h2>
-                    <p>“At this time, the venue is mandating Covid vaccines for all guests. 
-                        Please be sure that you have completed your vaccine series and all recommended boosters in order to attend this event. 
-                        Should there be any updates to this policy, we will update this page”</p>
+                    <Card>
+                        <Card.Header>
+                            <h2>Covid Information</h2>
+                        </Card.Header>
+                        <Card.Body>
+                            <p>At this time, the venue is mandating Covid vaccines for all guests.
+                                Please be sure that you have completed your vaccine series and all recommended boosters in order to attend this event.
+                                Should there be any updates to this policy, we will update this page</p>
+                        </Card.Body>
+                    </Card>
                 </Container>
             </div>
         )
