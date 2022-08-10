13de37e4ee63be3875f34283328e9efa49c93841
Mike Coletta <coletta.mike@gmail.com>
2022-08-10 14:36:47 -0700

    Added Alerts in Effect title and formatted card

--- src/pages/Alerts.js
+++ src/pages/Alerts.js
@@ -9,14 +9,15 @@ class Alerts extends React.Component {
             <div className="sectionDiv">
                 <Container className="contentContainer">
                     <br></br>
-                    <Card style={{ border: "none" }}>
+                    <Card>
                         <Card.Header style={{ backgroundColor: "white" }}>
-                            <h2>Covid Information</h2>
+                            <h2 className='alertsTitle'>Alerts in Effect</h2>
                         </Card.Header>
                         <Card.Body>
+                            <h4>COVID-19 Information</h4>
                             <p>At this time, the venue is mandating Covid vaccines for all guests.
                                 Please be sure that you have completed your vaccine series and all recommended boosters in order to attend this event.
-                                Should there be any updates to this policy, we will update this page</p>
+                                Should there be any updates to this policy, we will update this page.</p>
                         </Card.Body>
                     </Card>
                 </Container>
