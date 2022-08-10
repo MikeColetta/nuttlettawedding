f07f774aec09a65a536dd8fed8455d31f0695cbf
Mike Coletta <coletta.mike@gmail.com>
2022-08-09 17:42:26 -0700

    Changed Nuttletta to Nuttletta National Park.

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -8,13 +8,13 @@ function Banner() {
         <div className="bannerContainer">
             <div className="welcomeWrapper">
                 <Container className="welcomeMessage">
-                    <Row>
-                        <Col xs={2}><h1 >Nuttletta</h1></Col>
+                    <Row className="mt-3">
+                        <Col xs={4}><h1 >Nuttletta National Park</h1></Col>
                         <Col xs={2}>
-                            <p className="mb-0 mt-1">National Park</p>
+                            <p className="mb-0 mt-1">Eatonville</p>
                             <p>Washington</p>
-                            </Col>
-                        <Col s={8}></Col>
+                        </Col>
+                        <Col s={6}></Col>
                     </Row>
                 </Container>
             </div>
