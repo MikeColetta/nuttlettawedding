5679812614aee8cfed2db2b54259d3c9e3dba5b5
Mike Coletta <coletta.mike@gmail.com>
2022-08-10 14:27:41 -0700

    Added photo and food recommendations

--- src/pages/Eating.js
+++ src/pages/Eating.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Image } from 'react-bootstrap';
+import EatingPageImage from '../assets/images/EatingPage.jpg';
 
 
 class Eating extends React.Component {
@@ -13,12 +14,15 @@ class Eating extends React.Component {
                         <Card.Header style={{ backgroundColor: "white" }}>
                             <h2>Eating</h2>
                         </Card.Header>
+                        <Image src={EatingPageImage} style={{ height: "75%", width: "75%", margin: "auto" }} className="mt-2"></Image>
                         <Card.Body>
                             <p>If you're staying with us for the weekend, food will be provided. Stay tuned to this page for a menu coming soon!</p>
                             <br></br>
-                            <p>If you're not staying with us, here are some local recommendations:</p>
+                            <p>If you're not staying with us, here are some local recommendations in nearby Eatonville:</p>
                             <ul>
-                                <li>restaurant recommendations go on this list.</li>
+                                <li><a className="foodRecLink" href='https://www.eatbrunos.com/menus' target={"_blank"} rel="noreferrer">Bruno's Family Restaurant and Bar</a></li>
+                                <li><a className="foodRecLink" href='https://www.eatatcruisercafe.com/' target={"_blank"} rel="noreferrer">Cruiser Cafe</a></li>
+                                <li><a className="foodRecLink" href='http://shakentogo.com/' target={"_blank"} rel="noreferrer">Shaken</a></li>
                             </ul>
                         </Card.Body>
                     </Card>
