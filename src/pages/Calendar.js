2f3456b6fea24675ac06dde9447b1e496d1702b0
Mike Coletta <coletta.mike@gmail.com>
2022-08-10 14:27:21 -0700

    Added table

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Table } from 'react-bootstrap';
 
 class Calendar extends React.Component {
 
@@ -9,22 +9,45 @@ class Calendar extends React.Component {
                 <Container className="contentContainer">
                     <br></br>
                     <Card style={{ border: "none" }}>
-                    <Card.Header style={{ backgroundColor: "white" }}>
+                        <Card.Header style={{ backgroundColor: "white" }}>
 
-                    <h2>Schedule</h2>
-                    </Card.Header>
-                    <h4>Friday</h4>
-                    <li>Check-In</li>
-                    <li>BBQ</li>
-                    <h4>Saturday</h4>
-                    <li>Free Time</li>
-                    <li>Ceremony</li>
-                    <li>Reception</li>
-                    <h4>Sunday</h4>
-                    <li>Brunch</li>
-                    <li>Check-out</li>
+                            <h2>Schedule</h2>
+                        </Card.Header>
                     </Card>
+                    <Table className="mt-2" responsive="sm">
+                        <thead>
+                            <tr>
+                                <th>Time</th>
+                                <th>Friday, August 11th</th>
+                                <th>Saturday, August 12th</th>
+                                <th>Sunday, August 13th</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Morning</td>
+                                <td>N/A</td>
+                                <td>8:00am to 3:00pm - Free time</td>
+                                <td>8:00am to 11:00am - Breakfast and Goodbyes</td>
+
+                            </tr>
+                            <tr>
+                                <td>Afternoon</td>
+                                <td>N/A</td>
+                                <td>4:00pm - Wedding Ceremony</td>
+                                <td>N/A</td>
+
+                            </tr>
+                            <tr>
+                                <td>Evening</td>
+                                <td>5:00pm - Arrival and Cookout!</td>
+                                <td>6:00pm - Reception</td>
+                                <td>N/A</td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </Container>
+
             </div>
         )
     }
