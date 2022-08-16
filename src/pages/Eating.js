1a2f727455817f67825795b5e1364d05d24f05df
Mike Coletta <coletta.mike@gmail.com>
2022-08-15 21:43:17 -0700

    Added Katy's food recommendations

--- src/pages/Eating.js
+++ src/pages/Eating.js
@@ -15,10 +15,13 @@ class Eating extends React.Component {
                             <h2>Eating</h2>
                         </Card.Header>
                         <Image src={EatingPageImage} style={{ height: "75%", width: "75%", margin: "auto" }} className="mt-2"></Image>
+                        <br></br>
                         <Card.Body>
                             <p>If you're staying with us for the weekend, food will be provided. Stay tuned to this page for a menu coming soon!</p>
-                            <br></br>
-                            <p>If you're not staying with us, here are some local recommendations in nearby Eatonville:</p>
+                            <p>If you're not staying with us, here are some local recommendations:</p>
+                            <p>One of Katy's favorite places for pizza and a beverage after a hike at Mt. Rainier is the Rainer BaseCamp Bar and Grill located on the RMI campus.
+                                You can also grab a scoop or two of ice cream at the cafe in Whittaker's Bunkhouse</p>
+                            <p>Here are some local spots in nearby Eatonville:</p>
                             <ul>
                                 <li><a className="foodRecLink" href='https://www.eatbrunos.com/menus' target={"_blank"} rel="noreferrer">Bruno's Family Restaurant and Bar</a></li>
                                 <li><a className="foodRecLink" href='https://www.eatatcruisercafe.com/' target={"_blank"} rel="noreferrer">Cruiser Cafe</a></li>
