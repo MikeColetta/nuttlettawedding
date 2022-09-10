a29b51b281f639ccd85d039b1dea83c02e7b35d7
Mike Coletta <coletta.mike@gmail.com>
2022-09-10 00:33:47 -0700

    Made banner image responsive

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,20 +6,22 @@ import { Row, Col, Container } from "react-bootstrap";
 
 function Banner() {
     return (
-        <div className="bannerContainer">
-            <div className="welcomeWrapper">
-                <Container className="welcomeMessage">
-                    <Row className="mt-3">
-                        <Col xs={4}><Link to="/" className="bannerLink"><h1 >Nuttletta National Park</h1></Link></Col>
-                        <Col xs={2}>
-                            <p className="mb-0 mt-1">Eatonville</p>
-                            <p>Washington</p>
-                        </Col>
-                        <Col s={6}></Col>
-                    </Row>
-                </Container>
-            </div>
-        </div>
+        <section className="bannerContainer">
+            <figure className="bannerImage">
+                <div className="welcomeWrapper">
+                    <Container className="welcomeMessage">
+                        <Row className="mt-3">
+                            <Col xs={4}><Link to="/" className="bannerLink"><h1 >Nuttletta National Park</h1></Link></Col>
+                            <Col xs={2}>
+                                <p className="mb-0 mt-1">Eatonville</p>
+                                <p>Washington</p>
+                            </Col>
+                            <Col s={6}></Col>
+                        </Row>
+                    </Container>
+                </div>
+            </figure>
+        </section>
     )
 }
 
