d8cbcd4e3f2cf9734b15de00532da47868d85410
Mike Coletta <coletta.mike@gmail.com>
2023-05-29 23:28:57 -0700

    Updated registry link info

--- src/pages/Registry.js
+++ src/pages/Registry.js
@@ -14,12 +14,11 @@ class Registry extends React.Component {
                         </Card.Header>
                         <Card.Body>
                             <p>Gifts are not expected, but should you feel inclined, we've collected some options to help us celebrate our future together.
-                                Check out our registry on <a href='https://registry.theknot.com/katy-nuttman-mike-coletta-august-2023-wa/51681427' target={"_blank"} rel="noreferrer">The Knot</a>.
+                                Check out our registry on <a href='https://registry.theknot.com/katy-nuttman-mike-coletta-august-2023-wa/51681427' target={"_blank"} rel="noreferrer">The Knot (links to miscellaneous items, as well as our Target and Macy's registry)</a>.
                             </p>
                             <p>We are also registered directly at:</p>
                             <ul>
-                                <li><a href='https://www.myregistry.com/wedding-registry/katy-nuttman-and-mike-coletta-normandy-park-wa/3315647/giftlist' target={"_blank"} rel="noreferrer">REI</a></li>
-                                <li><a href='https://www.target.com/gift-registry/gift/nuttlettawedding' target={"_blank"} rel="noreferrer">Target</a></li>
+                                <li><a href='https://www.myregistry.com/wedding-registry/katy-nuttman-and-mike-coletta-normandy-park-wa/3315647' target={"_blank"} rel="noreferrer">MyRegistry (links to items from REI and additional Fiestaware)</a></li>
                             </ul>
                         </Card.Body>
                         <Image src="https://ik.imagekit.io/4lalrpwz0sy/RegistryPage_jihcT0_cW.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662797303616" style={{ height: "100%", width: "100%", margin: "auto" }} className="mt-2"></Image>
