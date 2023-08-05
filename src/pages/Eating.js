d565c37c0b4a3d7720c2023a88b60c0caa4d4e41
Mike Coletta <coletta.mike@gmail.com>
2023-08-05 16:04:03 -0700

    Added Menu

--- src/pages/Eating.js
+++ src/pages/Eating.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card, Image } from 'react-bootstrap';
+import { Container, Card, Image, Table } from 'react-bootstrap';
 
 class Eating extends React.Component {
 
@@ -15,7 +15,59 @@ class Eating extends React.Component {
                         <Image src="https://ik.imagekit.io/4lalrpwz0sy/Eating_4jcG2VmVE.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662795539001" style={{ height: "75%", width: "75%", margin: "auto" }} className="mt-2"></Image>
                         <br></br>
                         <Card.Body>
-                            <p>If you're staying with us for the weekend, food will be provided. Stay tuned to this page for a menu coming soon!</p>
+                            <Card.Header style={{ backgroundColor: "white" }}>
+                                <h4>Menu</h4>
+                            </Card.Header>
+                            <Table className="mt-2" responsive="sm">
+                                <thead>
+                                    <tr>
+                                        <th>Friday, August 11th</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Dinner - 6:30pm</td>
+                                        <td>Hamburgers and hotdogs, pasta salad, baked beans, berry crisp</td>
+                                    </tr>
+                                </tbody>
+                                <thead>
+                                    <tr>
+                                        <th>Saturday, August 12th</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Breakfast - 8:00am</td>
+                                        <td>Breakfast burritos and hashbrowns</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Lunch - 1:00pm</td>
+                                        <td>Grilled ham and cheese sandwich with tomato basil soup</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dinner - 6:15pm</td>
+                                        <td>Chicken alfredo lasagna with garlic bread</td>
+                                    </tr>
+                                </tbody>
+                                <thead>
+                                    <tr>
+                                        <th>Sunday, August 13th</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Brunch - 10:00am</td>
+                                        <td>French toast with berries and whipped cream</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            <Card.Header style={{ backgroundColor: "white" }}>
+                                <h4>Recommendations</h4>
+                            </Card.Header>
+                            <br></br>
                             <p>If you're not staying with us, here are some local recommendations:</p>
                             <p>One of Katy's favorite places for pizza and a beverage after a hike at Mt. Rainier is the Rainer BaseCamp Bar and Grill located on the RMI campus.
                                 You can also grab a scoop or two of ice cream at the cafe in Whittaker's Bunkhouse</p>
